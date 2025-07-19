2d21e037aa3020bc6a076bd908f0e60c80f3eb1f
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2025-07-19 21:28:18 +0300

    Just working with sets in Dart

---
+++ setwork.dart
@@ -0,0 +1,8 @@
+void main(){
+  var nisetie = {1,3,54,6,9,8};
+  nisetie.add(34);
+  nisetie.length;
+  var setulist =nisetie.map((kwanza) => kwanza%2).toList(); // Change the set to a list after modulo operation
+  print(setulist);
+
+}
