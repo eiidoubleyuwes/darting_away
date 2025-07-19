b17b13b861a4d1027f3f39d40126ec795d2bffd6
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2025-07-19 21:27:53 +0300

    Weird fizzBuzz is done,Now I know all about for each,and the terniary operator (?)

--- weirdfizzbuzz.dart
+++ weirdfizzbuzz.dart
@@ -1,3 +1,11 @@
-//Say we have an array of n numbers and we want to check whichones are divisible by 3 and 5
-var numbers = [2,3,4,4,5,5,6,5,4,56,78,33,34];
-var isDivicibleByThree = numbers.map((temp) => temp%3).toList();
+//Say we have an array of n numbers and we want to check which ones are divisible by 3 and 5(Basic FizzBuzz)
+void main(){
+  var numbers = [2,3,44,5,55,6,5,4,56,78,33,34];
+  numbers.addAll([15,45,69,79]);
+  //Using the == 0 I am converting to a boolean,then the ? : Combo is basically an if else statement
+  //Weirdly prints null,collected them in a list
+  // var fizzBuzz = numbers.map((a) => (a%5 == 0 && a%3 == 0) ? print('FizzBuzz') : (a%3 == 0) ? print('Fizz') : (a%5 == 0) ? print('Buzz') : print(a)); // This prints the null
+  var withoutNullFizzBuzz = numbers.forEach((a) => (a%5 == 0 && a%3 == 0) ? print('FizzBuzz') : (a%3 == 0) ? print('Fizz') : (a%5 == 0) ? print('Buzz') : print(a));
+  //No need to print,just print each
+  // print(fizzBuzz);
+}
